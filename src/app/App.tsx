737cb500ec41b14e0319c5949777844e44d8c255
Yeshwanth Reddy <yeshwanthreddycherla@gmail.com>
2026-02-17 10:56:51 +0530

    Fix Settings crash by wrapping app with ThemeProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,18 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { ThemeEngineProvider } from "./context/ThemeEngineContext";
+import { ThemeProvider } from "./context/ThemeContext";
 import { FeatureFlagsProvider } from "./context/FeatureFlagsContext";
 import { AuthProvider } from "./context/AuthContext";
 
 export default function App() {
   return (
     <ThemeEngineProvider>
-      <FeatureFlagsProvider>
-        <RouterProvider router={router} />
-      </FeatureFlagsProvider>
+      <ThemeProvider>
+        <FeatureFlagsProvider>
+          <RouterProvider router={router} />
+        </FeatureFlagsProvider>
+      </ThemeProvider>
     </ThemeEngineProvider>
   );
 }
